e3fb67720425241adbc6506ac4f7af7b08f97a1b
Patrick Benedict Lirio <86680909+PatrickLirio@users.noreply.github.com>
2026-01-17 11:39:36 +0800

    adding left files from namespace folder

--- NamespaceAndComment/Fruit/Strawberry.cs
+++ NamespaceAndComment/Fruit/Strawberry.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace NamespaceAndComment.Fruit
 {
-    internal class Class1
+    internal class Strawberry
     {
     }
 }
